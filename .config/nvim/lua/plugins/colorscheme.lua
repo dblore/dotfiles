0ee65355cda647c9aca21a272819db82f62994b9
Dan Blore <dan.blore@icloud.com>
2024-03-01 18:46:22 +0000

    updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
 }
